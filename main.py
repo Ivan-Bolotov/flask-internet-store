95677e86fabeaef7662d57da7d71144d9efa4e6c
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-04-07 20:18:17 +0300

    docker-compose.yml was added.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    context = dict()
+    context["title"] = "Main Page"
+    context["text"] = "Hello, World!"
+    return render_template("index.html", **context)
+
+
 if __name__ == '__main__':
-    print("initial commit!")
+    app.run(host="0.0.0.0", port=8080, debug=True)
