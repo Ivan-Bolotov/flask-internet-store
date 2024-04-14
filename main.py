dbee41647efc19b3e68950eb944dc68d4e3fee1b
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-04-14 21:06:20 +0300

    Models and database were added.

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, make_response, jsonify
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route("/")
 def index():
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "email@email.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
     context = dict()
     context["title"] = "Main Page"
     context["text"] = "Hello, World!"
     return render_template("index.html", **context)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/main_db.sqlite")
     app.run(host="0.0.0.0", port=8080, debug=True)
